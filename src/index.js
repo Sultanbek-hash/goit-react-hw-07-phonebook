e2cedefcadb080fc755391596c092f8aea4d9a12
Sultan <a_sultan_93@mail.ru>
2023-06-27 17:45:32 +0500

    123

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
-import { store, persistor } from './redux/store';
+import { store} from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
